b5536f6e33632f479f4a83bb1fbfba8af407d1c4
MattiaToscano <mattia1997ct@gmail.com>
2025-04-03 13:51:31 +0200

    [UPD] Recuperati elementi dal DOM

---
+++ script.js
@@ -0,0 +1,4 @@
+//Recuperiamo gli elementi dal DOM
+const emailList = document.getElementById('email-list');
+const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail?${items}=10`;
+
