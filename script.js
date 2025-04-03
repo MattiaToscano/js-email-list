eeee03228e31e57cd0c7b511df2df68ad11099ca
MattiaToscano <mattia1997ct@gmail.com>
2025-04-03 14:19:00 +0200

    [UPD] Stampate 10 mail in console.

--- script.js
+++ script.js
@@ -3,20 +3,23 @@ const emailList = document.getElementById('lista-email');
 
 
 // Funzione per generare una lista di email
+const mailArray = [];
 let items = 10;
 const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail?items=${items}`;
 
-axios.get(endpoint).then((response) => {
-    const emails = response.data.response;
-    console.log(emails);
-    // Iteriamo sull'array di email e creiamo gli elementi <li>
-    for (let i = 0; i < emails.length; i++) {
-        const email = emails[i];
-        const li = document.createElement('li');
-        li.innerHTML = email;
-        emailList.append(li);
-    }
-})
+for (let i = 0; i < items; i++) {
+    axios.get(endpoint).then((response) => {
+        const mailArray = response.data.response;
+        console.log(mailArray);
+    })}
+
+
+
+
+
+
+
+
 
 
 
