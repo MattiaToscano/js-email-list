6db600690c17954f15d9de39b8fa1175cce716b3
MattiaToscano <mattia1997ct@gmail.com>
2025-04-03 14:37:22 +0200

    [UPD] Bonus completato

--- script.js
+++ script.js
@@ -1,29 +1,40 @@
 //Recuperiamo gli elementi dal DOM
 const emailList = document.getElementById('lista-email');
-const button =document.getElementById('btn');
-
+const button = document.getElementById('btn');
+const spinner = document.getElementById('spinner');
 
 // Funzione per generare una lista di email
 const mailArray = [];
 let items = 10;
-const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail?items=${items}`;
+const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail`;
+let emailsLoaded = 0;
 
 //funzione per generare nuove mail tramite bottone
 button.addEventListener('click', function() {
     // Svuotiamo la lista esistente
     emailList.innerHTML = '';
+    
+    // Reset contatore
+    emailsLoaded = 0;
+    
     // Effettuiamo una chiamata API per ottenere le email
     for (let i = 0; i < items; i++) {
-        axios.get(endpoint).then((response) => {
-            const mailArray = response.data.response;
-            console.log(mailArray);
-            // Creiamo un elemento <li> per ogni email e lo aggiungiamo alla lista
-            const lista = document.createElement('li');
-            lista.innerHTML = mailArray;
-            emailList.append(lista);
-        })
-    }
-})
+        axios.get(endpoint)
+            .then((response) => {
+                const email = response.data.response;
+                console.log(email);
+                
+                // Aggiungiamo alla lista
+                const lista = document.createElement('li');
+                lista.className = 'list-group-item';
+                lista.innerHTML = email;
+                emailList.append(lista);
+                
+                emailsLoaded++;
+            })
+                      
+}    }   )                 
+ 
 
 
 
