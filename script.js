5d0e10b77dc679ef791944954cc8095bc0765fd3
MattiaToscano <mattia1997ct@gmail.com>
2025-04-03 13:58:31 +0200

    [UPD] Creata lista  singola e inserita singola mail

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 //Recuperiamo gli elementi dal DOM
-const emailList = document.getElementById('email-list');
-const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail?${items}=10`;
+const emailList = document.getElementById('lista-email');
+
+
+// Funzione per generare una lista di email
+let items = 10;
+const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail?items=${items}`;
+
+axios.get(endpoint).then((response) => {
+    const emails = response.data.response;
+    console.log(emails);
+    // Iteriamo sull'array di email e creiamo gli elementi <li>
+    for (let i = 0; i < emails.length; i++) {
+        const email = emails[i];
+        const li = document.createElement('li');
+        li.innerHTML = email;
+        emailList.append(li);
+    }
+})
+
+
 
